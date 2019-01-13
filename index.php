988f7611783ef31fc70fa797b351310c97a82c31
Ranj Bahadin <ranjbahadin@live.se>
2019-01-13 15:13:40 +0100

    adding css, js and framework

--- index.php
+++ index.php
@@ -5,28 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Start - Horoskop</title>
+    <link rel="stylesheet" href="./styling.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
     <?php
 
     ?>
     <h3>Skriv in din födelsenr, 6 siffror!</h3>
-    <form method="post" action="">  
+    <form action="addHoroscope.php" method="post">  
         <input type="text" name="birthNR" placeholder="YYMMDD" value="">
         <br>
         <span>
-        <button type="submit" name="saveHoro" value="">Spara mitt horoskop</button>
+        <button type="submit" name="saveHoro" value="" onclick="saveIT()">Spara mitt horoskop</button>
         </span>
         <span>
-        <button type="submit" name="updateHoro" value="">Uppdatera mitt horoskop</button>
+        <button type="submit" name="updateHoro" value="" onclick="updateIT()">Uppdatera mitt horoskop</button>
         </span>
         <span>
-        <button type="submit" name="eraseHoro" value="">Radera mitt horoskop</button>
+        <button type="submit" name="eraseHoro" value="" onclick="deleteIT()">Radera mitt horoskop</button>
         </span>
-
     </form>
-    <div id="myHoro">
+
+    <div class="container">
+        <h2>Ditt horoskop är</h2>
+        <div id="myHoro"></div>
     </div>
     
+    <script src="script.js"></script>
 </body>
 </html>
